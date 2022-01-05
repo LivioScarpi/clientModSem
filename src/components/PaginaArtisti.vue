8ef2dcca18fff4efe4b600b94bb3eb3f7c93b440
Livio Scarpinati <livio.scarpinati@gmail.com>
2022-01-05 18:23:16 +0100

    corretta query

--- src/components/PaginaArtisti.vue
+++ src/components/PaginaArtisti.vue
@@ -158,7 +158,7 @@ PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-select distinct ?artista ?evento ?nomeEvento ?dataEvento ?capienzaMassimaEvento ?strutturaEvento
+select distinct ?nomeArtista ?evento ?nomeEvento ?dataEvento ?capienzaMassimaEvento ?strutturaEvento
 where {
     {
     ?artista rdf:type meo:AgenteMusicale;
